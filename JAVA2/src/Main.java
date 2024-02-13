3f48f51b23b39b3ca1d6e4463dac5d802ea486d9
sebastian123k <159938783+sebastian123k@users.noreply.github.com>
2024-02-13 10:05:25 -0700

    Add files via upload
    Primera ventana con JFrame

---
+++ JAVA2/src/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		
+		
+		JFrame ventana  = new JFrame("example"); 
+		
+		
+		ventana.setVisible(true);
+		ventana.setSize(600, 400);
+		ventana.setLocation(100,100);
+		ventana.setLocationRelativeTo(null);
+		ventana.setResizable(true);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+
+	}
+
+}
